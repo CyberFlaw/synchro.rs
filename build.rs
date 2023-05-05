166fbed085edb8c464e9f37e1706b6cf791d8fd6
CyberFlaw <kannan.pappus@gmail.com>
2023-05-05 23:20:27 +0530

    stashing test migration to automake from cmake

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
-use cmake::Config;
+// use cmake::Config;
+use autotools::Config;
+
 use std::path::Path;
 use std::fs;
 
@@ -8,8 +10,7 @@ fn main() {
     let build_log_path = Path::new(".").join("runtime.log");
     fs::write(&build_log_path, format!("Target native lib path= {}\n", dst.display().to_string())).unwrap();
 
-    println!("cargo:rustc-link-search=native={}/bin", dst.display());
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=audioinput");
 
     println!("cargo:rerun-if-changed=audio-input/audio.c");
